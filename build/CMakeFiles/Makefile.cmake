61ba6af27150d8ce04bb11c221fb1ad1aa1667ad
43Clock <a89600@alunos.uminho.pt>
2021-03-10 22:27:19 +0000

    CMake

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -53,5 +53,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/projeto.dir/DependInfo.cmake"
+  "CMakeFiles/generator.dir/DependInfo.cmake"
   )
