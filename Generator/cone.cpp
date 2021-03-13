25501eb9f385adc107cc4c5f27ce1204adda1209
43Clock <a89600@alunos.uminho.pt>
2021-03-13 17:01:08 +0000

    Cone

--- Generator/cone.cpp
+++ Generator/cone.cpp
@@ -14,37 +14,23 @@ Forma *cone(float raio,float altura, int slices,int stacks){
         f->inserePonto(new Ponto(raio*sin(angle+division),0.0f,raio*cos(angle+division)));
         f->inserePonto(new Ponto(raio*sin(angle),0.0f,raio*cos(angle)));
         for(int j = 0;j<stacks;j++){
+            float raio_atual = raio - (float) ((raio / (float) stacks) * (float) (j + 1));
+            float raio_ant = raio - (float) ((raio / (float) stacks) * (float) (j));
+            float altura_atual = (float) ((altura / (float) stacks) * (float) (j+1));
+            float altura_ant = (float) ((altura / (float) stacks) * (float) (j));
             if(j!=stacks-1) {
-                f->inserePonto(new Ponto((raio - (float) ((raio / (float) stacks) * (float) (j + 1))) * sin(angle + division),
-                                            (float) ((altura / (float) stacks) * (float) (j + 1)),
-                                            (raio - (float) ((raio / (float) stacks) * (float) (j + 1))) * cos(angle + division)));
-                f->inserePonto(new Ponto((raio - (float) ((raio / (float) stacks) * (float) (j))) * sin(angle),
-                                            (float) ((altura / (float) stacks) * (float) (j)),
-                                            (raio - (float) ((raio / (float) stacks) * (float) (j))) * cos(angle)));
-                f->inserePonto(new Ponto((raio - (float) ((raio / (float) stacks) * (float) (j))) * sin(angle + division),
-                                            (float) ((altura / (float) stacks) * (float) (j)),
-                                            (raio - (float) ((raio / (float) stacks) * (float) (j))) * cos(angle + division)));
+                f->inserePonto(new Ponto(raio_atual * sin(angle + division),altura_atual,raio_atual * cos(angle + division)));
+                f->inserePonto(new Ponto(raio_ant * sin(angle),altura_ant,raio_ant * cos(angle)));
+                f->inserePonto(new Ponto(raio_ant * sin(angle + division),altura_ant,raio_ant * cos(angle + division)));
 
 
-                f->inserePonto(new Ponto((raio - (float) ((raio / (float) stacks) * (float) (j + 1))) * sin(angle + division),
-                                            (float) ((altura / (float) stacks) * (float) (j + 1)),
-                                            (raio - (float) ((raio / (float) stacks) * (float) (j + 1))) * cos(angle + division)));
-                f->inserePonto(new Ponto((raio - (float) ((raio / (float) stacks) * (float) (j + 1))) * sin(angle),
-                                            (float) ((altura / (float) stacks) * (float) (j + 1)),
-                                            (raio - (float) ((raio / (float) stacks) * (float) (j + 1))) * cos(angle)));
-                f->inserePonto(new Ponto((raio - (float) ((raio / (float) stacks) * (float) (j))) * sin(angle),
-                                            (float) ((altura / (float) stacks) * (float) (j)),
-                                            (raio - (float) ((raio / (float) stacks) * (float) (j))) * cos(angle)));
+                f->inserePonto(new Ponto(raio_atual * sin(angle + division),altura_atual,raio_atual * cos(angle + division)));
+                f->inserePonto(new Ponto(raio_atual * sin(angle),altura_atual,raio_atual * cos(angle)));
+                f->inserePonto(new Ponto(raio_ant * sin(angle),altura_ant,raio_ant * cos(angle)));
             } else{
-                f->inserePonto(new Ponto((raio - (float) ((raio / (float) stacks) * (float) (j + 1))) * sin(angle + division),
-                                            (float) ((altura / (float) stacks) * (float) (j + 1)),
-                                            (raio - (float) ((raio / (float) stacks) * (float) (j + 1))) * cos(angle + division)));
-                f->inserePonto(new Ponto((raio - (float) ((raio / (float) stacks) * (float) (j))) * sin(angle),
-                                            (float) ((altura / (float) stacks) * (float) (j)),
-                                            (raio - (float) ((raio / (float) stacks) * (float) (j))) * cos(angle)));
-                f->inserePonto(new Ponto((raio - (float) ((raio / (float) stacks) * (float) (j))) * sin(angle + division),
-                                            (float) ((altura / (float) stacks) * (float) (j)),
-                                            (raio - (float) ((raio / (float) stacks) * (float) (j))) * cos(angle + division)));
+                f->inserePonto(new Ponto(raio_atual * sin(angle + division),altura_atual,raio_atual * cos(angle + division)));
+                f->inserePonto(new Ponto(raio_ant * sin(angle),altura_ant,raio_ant * cos(angle)));
+                f->inserePonto(new Ponto(raio_ant * sin(angle + division),altura_ant,raio_ant * cos(angle + division)));
             }
         }
         angle += division;
